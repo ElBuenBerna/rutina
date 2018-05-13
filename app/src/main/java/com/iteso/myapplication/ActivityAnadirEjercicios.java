9b02d4d5882e05752e193df856d926c2779baa87
ElBuenBerna <bernardo.elizondo1@gmail.com>
2018-05-13 13:07:15 -0500

    Layouts crear rutina, elegir ejercicios y descripción de cada ejercicio.

---
+++ app/src/main/java/com/iteso/myapplication/ActivityAnadirEjercicios.java
@@ -0,0 +1,13 @@
+package com.iteso.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAnadirEjercicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anadir_ejercicios);
+    }
+}
